9c0f99f520f3d215014da0ae13fa2dfc1573bd7a
Ken Barrios <keniswaiting@gmail.com>
2017-06-06 11:31:51 -0500

    first pass at items within modal, still need to reframe modal itself

--- app/components/projects/GalleryItemModal.js
+++ app/components/projects/GalleryItemModal.js
@@ -32,19 +32,26 @@ class GalleryItemModal extends React.Component {
 					<img className='gallery-modal-image' src={require('../../images/' + proj.alias + '.jpeg')} alt={proj.title} />
 				</div>
 				<section className='gallery-modal-info'>
-					<div className='half-info'>
-						<div>
+					<div className='half-info top'>
+						<div className='modal-title'>
 							<h3>{proj.title}</h3>
 						</div>
-						<div>
+						<div className='modal-brief'>
 							<p>{proj.brief}</p>
 						</div>
 					</div>
-					<div className='half-info'>
+					<div className='half-info bottom'>
+						<div className='tech-list-div'>
+							<ul>
+							{ proj.tech.map((tech) => {
+								return(
+									<li>{tech}</li>
+								)
+							})}
+							</ul>
+						</div>
 						<div className='site-repo-div'>
 							<a href={proj.site} target='_blank'>Website</a>
-						</div>
-						<div>
 							<a href={proj.repo} target='_blank'>Github Repo</a>
 						</div>
 					</div>
