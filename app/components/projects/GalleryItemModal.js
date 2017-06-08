93b72c40866c64e9c216923bef383f5fdc1ac133
Ken Barrios <keniswaiting@gmail.com>
2017-06-07 20:07:44 -0500

    additinal changes to modal layout

--- app/components/projects/GalleryItemModal.js
+++ app/components/projects/GalleryItemModal.js
@@ -24,37 +24,39 @@ class GalleryItemModal extends React.Component {
 		let proj = this.props.project;
 		return(
 			<div className='gallery-item-modal'>
-				<aside className='back-button' onClick={this.handleClick.bind(this)}>
-					<a className='fa fa-times' aria-hidden='true'></a>
-				</aside>
-				<div className='gallery-modal-image-container'>
-					<img className='gallery-modal-image' src={require('../../images/' + proj.alias + '.jpeg')} alt={proj.title} />
+				<div className='gallery-item-modal-background'>
+					<aside className='back-button' onClick={this.handleClick.bind(this)}>
+						<a className='fa fa-times' aria-hidden='true'></a>
+					</aside>
+					<div className='gallery-modal-image-container'>
+						<img className='gallery-modal-image' src={require('../../images/' + proj.alias + '.jpeg')} alt={proj.title} />
+					</div>
+					<section className='gallery-modal-info'>
+						<div className='half-info top'>
+							<div className='modal-title'>
+								<h3>{proj.title}</h3>
+							</div>
+							<div className='modal-brief'>
+								<p>{proj.brief}</p>
+							</div>
+						</div>
+						<div className='half-info bottom'>
+							<div className='tech-list-div'>
+								<ul>
+								{ proj.tech.map((tech) => {
+									return(
+										<li key={tech}>{tech}</li>
+									)
+								})}
+								</ul>
+							</div>
+							<div className='site-repo-div'>
+								<a href={proj.site} target='_blank'>website</a>
+								<a href={proj.repo} target='_blank'>github repo</a>
+							</div>
+						</div>
+					</section>
 				</div>
-				<section className='gallery-modal-info'>
-					<div className='half-info top'>
-						<div className='modal-title'>
-							<h3>{proj.title}</h3>
-						</div>
-						<div className='modal-brief'>
-							<p>{proj.brief}</p>
-						</div>
-					</div>
-					<div className='half-info bottom'>
-						<div className='tech-list-div'>
-							<ul>
-							{ proj.tech.map((tech) => {
-								return(
-									<li key={tech}>{tech}</li>
-								)
-							})}
-							</ul>
-						</div>
-						<div className='site-repo-div'>
-							<a href={proj.site} target='_blank'>Website</a>
-							<a href={proj.repo} target='_blank'>Github Repo</a>
-						</div>
-					</div>
-				</section>
 			</div>
 		)
 	}
