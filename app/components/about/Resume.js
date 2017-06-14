0695cf89b5da78455d8d6e92ae3c4fb40ef66bb5
Ken Barrios <keniswaiting@gmail.com>
2017-06-14 16:10:11 -0500

    first pass at styling resume portion of about page

--- app/components/about/Resume.js
+++ app/components/about/Resume.js
@@ -5,40 +5,75 @@ import resume from '../../utilities/resume';
 
 class Resume extends React.Component {
 	render() {
+		const cv = resume.getResume();
+			
 		return(
 			<section className='base-section'>
 				<div className='resume-wrapper'>
-					<ResumeHeader />
+					<div className='resume-header'>
+						<div className='resume-header-contact'>
+							<h2>{cv.contact.name}</h2>
+							<h3>{cv.contact.location}</h3>
+							<p>{cv.contact.email}</p>
+						</div>
+						<div className='resume-header-avatar'>
+							<img className='avatar' src={require('../../images/' + cv.contact.img)} />
+						</div>
+					</div>
 
 					<div className='resume-body'>
 						<div className='resume-blurb'>
 							<article>
-								<p>I believe in these things...</p>
+								<span className='fa fa-quote-left'></span><p>{cv.blurb}</p><span className='fa fa-quote-right'></span>
 							</article>
 						</div>
-						<aside className='resume-tech-list'>
-							<h3>Tech</h3>
-							<ul>
-								<li>
-									reactjs
-								</li>
-							</ul>
-						</aside>
-						<div className='resume-work-list'>
-							<h3>Work</h3>
-							<ul>
-								<li>
-									Pierce & Assoc
-								</li>
-							</ul>
-						</div>
-						<div className='resume-school-list'>
-							<h3>School</h3>
-							<ul>
-								<li>
-									General Assembly
-								</li>
-							</ul>
+						<div className='resume-body-lists'> 
+							<aside className='resume-tech-list'>
+								<h3 className='resume-list-header text-align-left'>Tech</h3>
+								<ul>
+								{
+									cv.tech.map((t) => {
+										return(
+											<li key={t}>{t}</li>
+										)
+									})
+								}
+								</ul>
+							</aside>
+							<div className='resume-body-sub-lists'>
+								<div className='resume-work-list'>
+									<h3 className='resume-list-header text-align-right'>Work</h3>
+									<ul>
+									{
+										cv.work.map((w) => {
+											return(
+												<li key={w.id}>
+													<h4>{w.name}</h4>
+													<p>{w.period}</p>
+													<p>{w.focus}</p>	
+												</li>
+											)
+										})
+									}
+									</ul>
+								</div>
+								<div className='resume-school-list'>
+									<h3 className='resume-list-header text-align-right'>School</h3>
+									<ul>
+									{
+										cv.school.map((s) => {
+											return(
+												<li key={s.id}>
+													<h4>{s.name}</h4>
+													<p>{s.period}</p>
+													<p>{s.focus}</p>	
+												</li>
+											)
+										})
+									}
+									</ul>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
--- app/utilities/resume.js
+++ app/utilities/resume.js
@@ -2,25 +2,29 @@ const resume = {
 	contact: {
 		name: 'ken barrios',
 		location: 'lincoln square, chicago',
-		email: 'keniswaiting at gmail'
+		email: 'keniswaiting at gmail',
+		img: 'ken.jpg'
 	},
 	blurb: 'I want to make things, destroy things, then make more things',
 	tech: ['html', 'css', 'sass', 'javascript', 'typescript', 'jquery','angularjs', 'react'],
 	work: [
 		{
+			id: 1,
 			name: 'learnmetrics',
 			period: 'sept 2015 - jan 2017',
 			focus: 'front-end developer',
 			duties: 'used typescript and angularjs 1.x to build out models, routes, and components the latest version of the company app'
 		},
 		{
+			id: 2,
 			name: 'pierce & assoc',
 			period: 'april 2013 - may 2014',
-			focus: 'senior accounts receivable specialist',
+			focus: 'senior a/r specialist',
 			duties: 'tracked monthly income, generated monthly income reports, primary point person for communication with clients'
 		},
 		{
-			name: 'CCCSSF',
+			id: 3,
+			name: 'cccssf',
 			period: 'april 2011 - sept 2012',
 			focus: 'housing counselor',
 			duties: 'provided hour-long, over-the-phone counseling for debtors facing foreclosure, provided financial analysis and reviews of options'
@@ -28,16 +32,19 @@ const resume = {
 	],
 	school: [
 		{
+			id: 1,
 			name: 'general assembly',
 			period: 'april 2015 - june 2015',
 			focus: 'full stack web development',
 		},
 		{
+			id: 2,
 			name: 'general assembly - online',
 			period: 'oct 2014 - dec 2014',
 			focus: 'web design'
 		},
 		{
+			id: 3,
 			name: 'northeastern illinois univ',
 			period: 'sept 2002 - may 2009',
 			focus: 'history & political science'
