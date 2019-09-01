ebf8995bba95667937ac290afa0f045b2097eb6f
Ken Barrios <keniswaiting@gmail.com>
2019-09-01 11:26:24 -0500

    begins updating h1 - pending rest of typography updates

--- app/components/landing/Title.js
+++ app/components/landing/Title.js
@@ -3,8 +3,11 @@ import React from 'react';
 function Title() {
 	return (
 		<div className='landing-header'>
-			<h1>ken</h1>
-			<h1>barrios</h1>
+			<h1>
+				Ken
+				<br />
+				Barrios
+			</h1>
 			<h2>front end developer</h2>
 
 			<p>[site under reconstruction]</p>
